668fe5503414d6e1613df88e8b501b7c931f2813
Stanislav Poslavsky <stvlpos@mail.ru>
2014-06-24 13:35:32 +0400

    Git test

--- src/main/java/com/milaboratory/core/io/sequence/SingleRead.java
+++ src/main/java/com/milaboratory/core/io/sequence/SingleRead.java
@@ -3,6 +3,7 @@ package com.milaboratory.core.io.sequence;
 import com.milaboratory.core.sequence.NSequenceWithQuality;
 
 /**
+ * Single read
  * @author Dmitry Bolotin
  * @author Stanislav Poslavsky
  */
