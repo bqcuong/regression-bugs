c4a5f6f8ef8a85ea54f4623ee3fd828590cb16b6
Dmitriy Bolotin <bolotin.dmitriy@gmail.com>
2014-08-26 17:50:29 +0400

    Fix for wildcards.

--- src/main/java/com/milaboratory/core/sequence/WildcardMapBuilder.java
+++ src/main/java/com/milaboratory/core/sequence/WildcardMapBuilder.java
@@ -2,11 +2,17 @@ package com.milaboratory.core.sequence;
 
 import gnu.trove.map.hash.TCharObjectHashMap;
 
+import static java.lang.Character.toLowerCase;
+import static java.lang.Character.toUpperCase;
+
 final class WildcardMapBuilder {
     final TCharObjectHashMap<WildcardSymbol> map = new TCharObjectHashMap<>();
 
     WildcardMapBuilder addWildcard(char c, byte... codes) {
-        map.put(c, new WildcardSymbol(c, codes));
+        char uC = toUpperCase(c);
+        WildcardSymbol wcSymbol = new WildcardSymbol(uC, codes);
+        map.put(toLowerCase(c), wcSymbol);
+        map.put(uC, wcSymbol);
         return this;
     }
 
