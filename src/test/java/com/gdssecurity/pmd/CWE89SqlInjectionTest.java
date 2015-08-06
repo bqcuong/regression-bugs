97d3f8d98d55ea58305d14ba97cbf279a981d6b4
alberto <infjaf@gmail.com>
2015-08-06 23:42:01 +0200

    Constructor for tests

---
+++ src/test/java/com/gdssecurity/pmd/CWE89SqlInjectionTest.java
@@ -0,0 +1,18 @@
+package com.gdssecurity.pmd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CWE89SqlInjectionTest {
+
+	public CWE89SqlInjectionTest() {
+		super();
+	}
+
+	@Test
+	public void test1() throws Exception {
+		Assert.assertEquals(1, PMDRunner.run("src/test/java/resources/cwe89sqlinjection/sqlinjection1",
+				PMDRunner.RULESET_SQL_INJECTION));
+	}
+
+}
--- src/test/java/com/gdssecurity/pmd/CWE935NoAccessControlTest.java
+++ src/test/java/com/gdssecurity/pmd/CWE935NoAccessControlTest.java
@@ -4,8 +4,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CWE935NoAccessControlTest {
+	
+	public CWE935NoAccessControlTest() {
+		super();
+	}
+
 	@Test
 	public void test1() throws Exception {
-		Assert.assertEquals(2, PMDRunner.run("src/test/java/resources/cwe935noaccesscontrol/access01", PMDRunner.RULESET_ACCESS));
+		Assert.assertEquals(2,
+				PMDRunner.run("src/test/java/resources/cwe935noaccesscontrol/access01", PMDRunner.RULESET_ACCESS));
 	}
 }
