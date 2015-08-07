0c1f0f0b7b492170ca815068abd31a71702d7f9f
alberto <infjaf@gmail.com>
2015-08-07 21:57:54 +0200

    more tests for sql injection

--- src/test/java/com/gdssecurity/pmd/CWE89SqlInjectionTest.java
+++ src/test/java/com/gdssecurity/pmd/CWE89SqlInjectionTest.java
@@ -3,16 +3,47 @@ package com.gdssecurity.pmd;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class CWE89SqlInjectionTest {
 
+	private static String DIR = "src/test/java/resources/cwe89sqlinjection/";
 	public CWE89SqlInjectionTest() {
 		super();
 	}
 
+	private static int run (String file) throws Exception {
+		return PMDRunner.run(DIR + file, PMDRunner.RULESET_SQL_INJECTION);
+	}
+
+	
 	@Test
-	public void test1() throws Exception {
-		Assert.assertEquals(1, PMDRunner.run("src/test/java/resources/cwe89sqlinjection/TestSqliServlet.java",
-				PMDRunner.RULESET_SQL_INJECTION));
+	public void simpleTest() throws Exception {
+		Assert.assertEquals(1, run("TestSqliServlet.java"));
+	}
+	
+	@Test
+	public void testDontMissAFunction () throws Exception {
+		Assert.assertEquals(20, run("SqliDontMissAFunction.java"));		
+	}
+	
+	@Test
+	public void testFieldsAreTainted () throws Exception {
+		Assert.assertEquals(2, run("SqliFieldsAreTainted.java"));
+	}
+	
+	@Test
+	public void testMethodsParametersTainted () throws Exception {
+		Assert.assertEquals(1, run("SqliMethodParametersAreTainted.java"));
+	}
+	
+	@Test
+	public void testConstFieldsAreSafe() throws Exception {
+		Assert.assertEquals(0, run("SqliConstFieldsAreSafe.java"));
+	}
+	
+	@Test
+	public void testNumericTypesAreSafe() throws Exception {
+		Assert.assertEquals(0, run("SqliNumericTypesAreSafe.java"));
 	}
 
 }
---
+++ src/test/java/resources/cwe89sqlinjection/SqliNumericTypesAreSafe.java
@@ -0,0 +1,16 @@
+package resources.cwe89sqlinjection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SqliNumericTypesAreSafe {
+
+	private int safeField = 1;
+	
+	public void execute(Connection con, int safeParameter) throws SQLException {
+		con.prepareStatement("select * from users where id = " + safeParameter);
+	}
+	public void execute(Connection con) throws SQLException {
+		con.prepareStatement("select * from users where id = " + safeField);
+	}
+}
