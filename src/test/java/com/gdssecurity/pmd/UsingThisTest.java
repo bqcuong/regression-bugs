ce162324bb7524b5aa4549689c729f991255b740
alberto <infjaf@gmail.com>
2015-12-16 23:52:05 +0100

    Better method name

--- src/test/java/com/gdssecurity/pmd/UsingThisTest.java
+++ src/test/java/com/gdssecurity/pmd/UsingThisTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class UsingThisTest {
 
 	@Test
-	public void testTernary () throws Exception {
+	public void testUsingThis () throws Exception {
 		Assert.assertEquals(1, PMDRunner.run("src/test/java/resources/others/UsingThis.java", PMDRunner.RULESET_SQL_INJECTION));
 	}
 }
