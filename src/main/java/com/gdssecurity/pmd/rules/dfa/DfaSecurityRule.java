d2554b55e5d9211034e826fbc6827db41f1675f3
albfernandez <infjaf@gmail.com>
2016-03-03 22:39:42 +0100

    Fixes detection of complex expressions, limit dataflows to compensate more computations

--- src/main/java/com/gdssecurity/pmd/rules/dfa/DfaSecurityRule.java
+++ src/main/java/com/gdssecurity/pmd/rules/dfa/DfaSecurityRule.java
@@ -119,7 +119,7 @@ public class DfaSecurityRule extends BaseSecurityRule implements Executable {
 
 	private List<DataFlowNode> additionalDataFlowNodes = new ArrayList<DataFlowNode>();
 
-	private static final int MAX_DATAFLOWS = 250;
+	private static final int MAX_DATAFLOWS = 40;
 	private boolean generator = false;
 	private boolean initialized = false;
 
