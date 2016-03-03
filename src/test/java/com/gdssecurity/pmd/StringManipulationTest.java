aeea9f107f8e12e12b9c7f2f74ce6123e7c9a0a8
albfernandez <infjaf@gmail.com>
2016-03-03 22:39:52 +0100

    Reenable test

--- src/test/java/com/gdssecurity/pmd/StringManipulationTest.java
+++ src/test/java/com/gdssecurity/pmd/StringManipulationTest.java
@@ -1,12 +1,10 @@
 package com.gdssecurity.pmd;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class StringManipulationTest {
 	@Test
-	@Ignore("Not yet implemented")
 	public void testStringManipulation () throws Exception {
 		Assert.assertEquals(3, PMDRunner.run("src/test/java/resources/others/StringManipulationExample.java", PMDRunner.RULESET_SQL_INJECTION));
 	}
