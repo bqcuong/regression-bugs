e98a183104cd2b325d0ece7d39f25d0cde6a918c
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2016-07-15 12:23:33 +0300

    Better pretty printer for JSON serialisation.

--- src/main/java/com/milaboratory/util/GlobalObjectMappers.java
+++ src/main/java/com/milaboratory/util/GlobalObjectMappers.java
@@ -15,16 +15,22 @@
  */
 package com.milaboratory.util;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.IOException;
 
 /**
  * Created by dbolotin on 24.01.14.
  */
 public class GlobalObjectMappers {
     public static final ObjectMapper ONE_LINE = new ObjectMapper();
-    public static final ObjectMapper PRETTY = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    public static final ObjectMapper PRETTY = new ObjectMapper()
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .setDefaultPrettyPrinter(new DefaultPrettyPrinter1());
 
     public static String toOneLine(Object object) throws JsonProcessingException {
         String str = GlobalObjectMappers.ONE_LINE.writeValueAsString(object);
@@ -33,5 +39,24 @@ public class GlobalObjectMappers {
             throw new RuntimeException("Internal error.");
 
         return str;
+    }
+
+    public static final class DefaultPrettyPrinter1 extends DefaultPrettyPrinter {
+        public DefaultPrettyPrinter1() {
+        }
+
+        public DefaultPrettyPrinter1(DefaultPrettyPrinter base) {
+            super(base);
+        }
+
+        @Override
+        public void writeObjectFieldValueSeparator(JsonGenerator jg) throws IOException {
+            jg.writeRaw(": ");
+        }
+
+        @Override
+        public DefaultPrettyPrinter createInstance() {
+            return new DefaultPrettyPrinter1(this);
+        }
     }
 }
