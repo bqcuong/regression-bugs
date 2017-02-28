885e796a2df99dac0e2aa23b1a1c9a6c450acf4e
Stanislav Poslavsky <stvlpos@mail.ru>
2017-02-28 17:36:22 +0300

    Enhancement in TempFileManager

--- src/main/java/com/milaboratory/util/TempFileManager.java
+++ src/main/java/com/milaboratory/util/TempFileManager.java
@@ -1,10 +1,13 @@
 package com.milaboratory.util;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import org.apache.commons.math3.random.Well44497b;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.security.SecureRandom;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -12,22 +15,37 @@ public class TempFileManager {
     private static volatile String prefix = "milib_";
     private static final AtomicBoolean initialized = new AtomicBoolean(false);
     static final ConcurrentHashMap<String, File> createdFiles = new ConcurrentHashMap<>();
+    private static final RandomDataGenerator privateRandom = new RandomDataGenerator(new Well44497b());
 
     public static void setPrefix(String prefix) {
         TempFileManager.prefix = prefix;
     }
 
+    public static void seed(long seed) {
+        synchronized ( privateRandom ){
+            privateRandom.getRandomGenerator().setSeed(seed);
+        }
+    }
+
+    private static void ensureInitialized() {
+        if (initialized.compareAndSet(false, true)) {
+            // Adding delete files shutdown hook on the very firs execution of getTempFile()
+            Runtime.getRuntime().addShutdownHook(new Thread(new RemoveAction(), "DeleteTempFiles"));
+            seed(System.nanoTime() + 17 * (new SecureRandom()).nextLong());
+        }
+    }
+
     public static File getTempFile() {
         try {
-            if (initialized.compareAndSet(false, true))
-                // Adding delete files shutdown hook on the very firs execution of getTempFile()
-                Runtime.getRuntime().addShutdownHook(new Thread(new RemoveAction(), "DeleteTempFiles"));
+            ensureInitialized();
 
             File file;
             String name;
 
             do {
-                name = prefix + RandomUtil.getThreadLocalRandomData().nextHexString(40);
+                synchronized ( privateRandom ){
+                    name = prefix + privateRandom.nextHexString(40);
+                }
                 file = Files.createTempFile(name, null).toFile();
             } while (createdFiles.putIfAbsent(name, file) != null);
 
@@ -42,15 +60,15 @@ public class TempFileManager {
 
     public static File getTempDir() {
         try {
-            if (initialized.compareAndSet(false, true))
-                // Adding delete files shutdown hook on the very firs execution of getTempFile()
-                Runtime.getRuntime().addShutdownHook(new Thread(new RemoveAction(), "DeleteTempFiles"));
+            ensureInitialized();
 
             File dir;
             String name;
 
             do {
-                name = prefix + RandomUtil.getThreadLocalRandomData().nextHexString(40);
+                synchronized ( privateRandom ){
+                    name = prefix + privateRandom.nextHexString(40);
+                }
                 dir = Files.createTempDirectory(name).toFile();
             } while (createdFiles.putIfAbsent(name, dir) != null);
 
