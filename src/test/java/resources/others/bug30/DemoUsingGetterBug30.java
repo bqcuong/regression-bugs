cf3347b30665781fe3230ff5c8e6987db3bff864
Alberto Fernandez <infjaf@gmail.com>
2017-10-10 19:38:41 +0200

    code example simplified for easy debugging

--- src/test/java/resources/others/bug30/DemoUsingGetterBug30.java
+++ src/test/java/resources/others/bug30/DemoUsingGetterBug30.java
@@ -7,20 +7,12 @@ public class DemoUsingGetterBug30 {
 	
 	private Connection connection;
 	
-	public DemoUsingGetterBug30() {
-		super();
-	}
-
-	public Connection getConnection() {
-		return connection;
-	}
-
-	public void setConnection(Connection connection) {
-		this.connection = connection;
-	}
-
 	public void badMethod(String badParam) throws SQLException {		
 		// This should fire a violation
 		getConnection().prepareStatement("select * from table where field='" + badParam + "'");
+	}
+	
+	public Connection getConnection() {
+		return connection;
 	}
 }
