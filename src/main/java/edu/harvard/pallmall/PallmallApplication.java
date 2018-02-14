0a06f5bea855bb31c8ac26921c0846d5e6ca258c
Stephen Benjamin <stephen@redhat.com>
2018-02-14 16:12:19 -0500

    Initial commit

---
+++ src/main/java/edu/harvard/pallmall/PallmallApplication.java
@@ -0,0 +1,12 @@
+package edu.harvard.pallmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PallmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PallmallApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/harvard/pallmall/PallmallApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.harvard.pallmall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PallmallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
