479c18965baf5ab64bebf9b0caa504817cf312c7
Stephen Benjamin <stephen@redhat.com>
2018-02-16 17:29:51 -0500

    Add Doctor repository

---
+++ src/main/java/edu/harvard/pallmall/DoctorRepository.java
@@ -0,0 +1,12 @@
+package edu.harvard.pallmall;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "doctors", path = "doctors")
+public interface DoctorRepository extends PagingAndSortingRepository<Doctor, Long> {
+    List<Doctor> findByLastName(@Param("lastName") String lastName);
+}
