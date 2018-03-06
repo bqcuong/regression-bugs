a0fc26d3bd787a525177893508b193158c7c9aab
Stephen Benjamin <stephen@redhat.com>
2018-03-05 21:26:32 -0500

    Rename pallmall test app to H2MS

--- src/main/java/edu/harvard/h2ms/web/controller/SecurityConfig.java
+++ src/main/java/edu/harvard/h2ms/web/controller/SecurityConfig.java
@@ -1,4 +1,4 @@
-package edu.harvard.pallmall.web.controller;
+package edu.harvard.h2ms.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
