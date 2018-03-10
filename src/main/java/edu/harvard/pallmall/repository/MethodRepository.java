e2c7ad01183ddce318f32328eca093c41f84ecce
Stephen Benjamin <stephen@redhat.com>
2018-03-10 13:16:29 -0500

    Add end points for hand washing methods

---
+++ src/main/java/edu/harvard/pallmall/repository/MethodRepository.java
@@ -0,0 +1,13 @@
+package edu.harvard.h2ms.repository;
+
+import java.util.List;
+
+import edu.harvard.h2ms.domain.core.Method;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "methods", path = "methods")
+public interface MethodRepository extends PagingAndSortingRepository<Method, Long> {
+    List<Method> findByName(@Param("name") String name);
+}
