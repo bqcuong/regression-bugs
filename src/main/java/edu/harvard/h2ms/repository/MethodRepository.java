edebf70e52b027e06e48c24b383b95733b6c7b1d
Stephen Benjamin <stephen@redhat.com>
2018-03-11 09:22:08 -0400

    Automatic swagger documentation

--- src/main/java/edu/harvard/h2ms/repository/MethodRepository.java
+++ src/main/java/edu/harvard/h2ms/repository/MethodRepository.java
@@ -3,6 +3,8 @@ package edu.harvard.h2ms.repository;
 import java.util.List;
 
 import edu.harvard.h2ms.domain.core.Method;
+import io.swagger.annotations.Api;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
