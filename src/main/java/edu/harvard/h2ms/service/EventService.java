07840b6615d7974a7cdd2c6e9b06390e128b4e48
MichaelDelaney <michaeldelaney@g.harvard.edu>
2018-03-21 15:50:47 -0400

    Added API - Num of Observations grouped by Week, Month, Year, Quarter

--- src/main/java/edu/harvard/h2ms/service/EventService.java
+++ src/main/java/edu/harvard/h2ms/service/EventService.java
@@ -1,44 +1,22 @@
 package edu.harvard.h2ms.service;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Map;
 
 import edu.harvard.h2ms.domain.core.Event;
-import edu.harvard.h2ms.repository.EventRepository;
+import org.json.JSONObject;
 
 /**
- * Contains the business logic and call methods in the repository layer
- * https://www.codebyamir.com/blog/create-rest-api-with-spring-boot
+ * Event Service ...
  */
-@Service
-public class EventService {
-	
-	@Autowired
-	private EventRepository eventRepository;
-	
-	//
-	public List<Event> getAllEvents() {
-		List<Event> events = new ArrayList<Event>();
-		eventRepository.findAll().forEach(events::add);
-		return events;
-	}
-	
-	public Event getEvent(Long id) {
-		return eventRepository.findOne(id);
-	}
-	
-	public Event addEvent(Event event) {
-		eventRepository.save(event);
-		return event;
-	}
-	
-	public void deleteEvent(Long id) {
-		eventRepository.delete(id);
-	}
-	
-	
+public interface EventService {
+
+	/**
+	 * Retrieves all events in H2MS systems and returns results
+	 * grouped by a specified timeframe (ie. week, month, year, quarter)
+	 * @param timeframe - week, month, year, quarter
+	 * @return
+	 */
+	Map<String, Long> findEventCountByTimeframe(String timeframe);
 
 }
