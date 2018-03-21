9061b045f887a6d9dccbedafe85c6dc7ca6ace8b
Wesley Skillern <wskillern@g.harvard.edu>
2018-03-20 20:24:07 -0400

    login page ui draft

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import {
+    MatButtonModule,
+    MatCardModule, MatCheckboxModule, MatDialogModule, MatDividerModule, MatFormFieldModule,
+    MatInputModule
+} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+      MatInputModule,
+      BrowserAnimationsModule,
+      MatCardModule,
+      MatCheckboxModule,
+      MatDividerModule,
+      MatButtonModule,
+      MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
