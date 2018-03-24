939bbf31fd655d1b57a0b9c9cd3cb01e784748a3
MichaelDelaney <michaeldelaney@g.harvard.edu>
2018-03-24 00:37:46 -0400

    Refactored Avg  Compliance Impl

--- src/main/java/edu/harvard/h2ms/service/UserServiceImpl.java
+++ src/main/java/edu/harvard/h2ms/service/UserServiceImpl.java
@@ -6,8 +6,6 @@ import edu.harvard.h2ms.repository.EventRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.math.RoundingMode;
-import java.text.NumberFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,6 +14,7 @@ import org.springframework.stereotype.Service;
 import edu.harvard.h2ms.domain.core.User;
 import edu.harvard.h2ms.repository.UserRepository;
 import org.springframework.transaction.annotation.Transactional;
+import static edu.harvard.h2ms.service.utils.H2msRestUtils.calculateAverage;
 import static java.lang.Boolean.TRUE;
 
 @Service("userService")
@@ -25,88 +24,69 @@ public class UserServiceImpl implements UserService {
 
 	final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
+	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
-	public void setUserRepository(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-
 	private EventRepository eventRepository;
-
-	@Autowired
-	public void setEventRepository(EventRepository eventRepository) {
-		this.eventRepository = eventRepository;
-	}
 
 	@Override
 	@Transactional(readOnly=true)
 	public Map<String, Double> findAvgHandWashCompliance() {
 
 		// Fetches all users from H2MS database
-		Iterable<User> results = userRepository.findAll();
-		if(results == null)
-			return null;
-		List<User> users = Lists.newArrayList(results);
+		List<User> users = Lists.newArrayList(userRepository.findAll());
 		log.info("No. of users found: {}", users.size());
+		if(users.isEmpty()) { return null; }
 
 		// Determines all the distinct types of users
-		List<String> distinctUserTypes = users.stream()
-				.map(User::getType)
-				.collect(Collectors.toList());
+		List<String> distinctUserTypes = users.stream().map(User::getType).collect(Collectors.toList());
 		log.info("There are {} distinct user types ", distinctUserTypes.size());
-
-		// No user types found
-		if(distinctUserTypes.isEmpty())
-			return null;
+		if(distinctUserTypes.isEmpty()) { return null; }
 
 		// Fetches all events from the H2MS database
 		List<Event> events = Lists.newArrayList(eventRepository.findAll());
 		log.info("No. of events found: {}", events.size());
+		if(events.isEmpty()) { return null; }
 
-		// No events found
-		if(events.isEmpty())
-			return null;
+		return calcAvgHandWashCompByDistinctUserType(distinctUserTypes, events);
+	}
+
+	public Map<String, Double> calcAvgHandWashCompByDistinctUserType(List<String> distinctUserTypes, List<Event> events){
 
 		Map<String, Double> dataMap = new HashMap<String, Double>();
-		long totalPopulation;
-		long washedPopulation;
+		Double totalPopulation;
+		Double washedPopulation;
 
 		for (String type : distinctUserTypes) {
-			if(type == null)
-				continue;
+			if(type == null) { continue; }
+
 			// Retrieves the total count of events per user type
 			Stream<Event> eventStream = events.stream().filter(event -> event.getSubject().getType().equals(type));
-			if (eventStream == null) {
+			if (eventStream != null) {
+				totalPopulation = Double.valueOf(eventStream.count());
+			} else{
 				log.warn("There are no events from user type {}", type);
 				continue;
 			}
-			totalPopulation = eventStream.count();
 
-			// Retrieves the count of events where user type took opportunity to wash hands
-			eventStream = events.stream().filter(event -> event.getSubject().getType().equals(type) &&
-										         event.getWashed()==TRUE);
-			if(eventStream == null) {
-				log.warn("No users of any user type {} took the opportunity to wash their hands.", type);
-				continue;
-			}
-			washedPopulation = eventStream.count();
-
-			if(Long.valueOf(washedPopulation)==null) {
+			// Retrieves the total count of events per user type where user took opportunity to wash hands
+			eventStream = events.stream().filter(event -> event.getSubject().getType().equals(type) && event.getWashed()==TRUE);
+			if(eventStream != null) {
+				washedPopulation = Double.valueOf(eventStream.count());
+			} else {
 				log.warn("No users of any user type {} took the opportunity to wash their hands.", type);
 				continue;
 			}
 
-			// Calculates the percent of time user type took opportunity to hand wash
-			Double result = Double.valueOf(washedPopulation)/Double.valueOf(totalPopulation);
+			// Calculates the average hand wash compliance for a given type
+			if(totalPopulation != null && totalPopulation != null) {
+				dataMap.put(type, calculateAverage(washedPopulation, totalPopulation));
+			} else {
+				log.warn("No users of any user type {} took the opportunity to wash their hands.", type);
+				continue;
+			}
 
-			// Format for response
-			NumberFormat nf= NumberFormat.getInstance();
-			nf.setMaximumFractionDigits(2);
-			nf.setMinimumFractionDigits(2);
-			nf.setRoundingMode(RoundingMode.HALF_UP);
-
-			dataMap.put(type, Double.valueOf(nf.format(result)));
 		}
 
 		return dataMap;
