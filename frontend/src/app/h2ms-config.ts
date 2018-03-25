be7f154ba88ce277ff3c056538e950853e97bad4
ben <benjenkinsv95@gmail.com>
2018-03-25 12:31:39 -0400

    * Adding a Config toggle button to the navmenu (only visible when not in production)

--- frontend/src/app/h2ms-config.ts
+++ frontend/src/app/h2ms-config.ts
@@ -3,7 +3,4 @@ import {Config} from './config';
 /**
  * Configuration for Hand Hygiene app.
  */
-export const CONFIG: Config = {
-    navbarTitle: 'H2MS',
-    websiteUrl: 'www.h2ms.org'
-};
+export const CONFIG: Config = new Config('H2MS', 'www.h2ms.org');
