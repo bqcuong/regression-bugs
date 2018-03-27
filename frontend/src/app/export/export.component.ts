2777bd15d852e5dc1d31a786934ec0065eb66cde
Kevin Trosinski <ktrosinski@g.harvard.edu>
2018-03-26 20:48:58 -0400

    Removed even more unused stuff.

--- frontend/src/app/export/export.component.ts
+++ frontend/src/app/export/export.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { saveAs } from 'file-saver/FileSaver';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
