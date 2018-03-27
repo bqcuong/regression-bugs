d9c6a2a4af8c4b5e9c1c4e0cbd497a6be81e7eda
shinwachi <shinwachi@gmail.com>
2018-03-27 12:42:26 -0400

    Update EmailServiceImpl.java
    Added PropertySources import statement.

--- src/main/java/edu/harvard/h2ms/service/EmailServiceImpl.java
+++ src/main/java/edu/harvard/h2ms/service/EmailServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
