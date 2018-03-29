0914d7f34afd6f0cab73445534276096d77fd685
shinwachi <shinwachi@gmail.com>
2018-03-28 20:58:15 -0400

    Moved Greenmail startup to EmailServiceImpl, and also created a propertySource value called emailServerType so the server can optionally start up the Greenmail server.

--- src/main/java/edu/harvard/h2ms/H2MSRestAppInitializer.java
+++ src/main/java/edu/harvard/h2ms/H2MSRestAppInitializer.java
@@ -5,9 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.ServerSetupTest;
-
 /**
  * H2MS Rest Application
  * @version 1.0
@@ -16,11 +13,10 @@ import com.icegreen.greenmail.util.ServerSetupTest;
 public class H2MSRestAppInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(H2MSRestAppInitializer.class);
-	@SuppressWarnings("unused")
+	
 	
 	public static void main(String[] args) {
-		GreenMail greenMail = new GreenMail(ServerSetupTest.SMTP);
-        greenMail.start();
+
 		SpringApplication.run(H2MSRestAppInitializer.class, args);
 	}
 }
--- src/main/java/edu/harvard/h2ms/service/EmailServiceImpl.java
+++ src/main/java/edu/harvard/h2ms/service/EmailServiceImpl.java
@@ -3,6 +3,9 @@ package edu.harvard.h2ms.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.mail.SimpleMailMessage;
@@ -11,15 +14,16 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
+
 @Component
 @Service("emailService")
 @PropertySources({
-	@PropertySource(value = "classpath:mailserver.properties.template", ignoreResourceNotFound = true),
 	@PropertySource(value = "classpath:mailserver.mock.properties", ignoreResourceNotFound = true),
 	@PropertySource(value = "classpath:mailserver.properties", ignoreResourceNotFound = true)
-	
 })
-public class EmailServiceImpl implements EmailService {
+public class EmailServiceImpl implements EmailService, ApplicationListener<ApplicationReadyEvent> {
 	
 	final Logger log = LoggerFactory.getLogger(EmailServiceImpl.class);
 	
@@ -34,6 +38,30 @@ public class EmailServiceImpl implements EmailService {
 		
 		log.info("sending email to: "+email);
 		mailSender.send(email);
+	}
+	
+	
+	
+	@Value("${emailServerType}")
+	private String emailServerType;
+	
+	static private GreenMail greenMail = null;
+	/**
+	 * http://blog.netgloo.com/2014/11/13/run-code-at-spring-boot-startup/
+     * This event is executed as late as conceivably possible to indicate that 
+     * the application is ready to service requests.
+     */
+	@Override
+	public void onApplicationEvent(final ApplicationReadyEvent event) {
+		
+		if(greenMail == null && emailServerType.equalsIgnoreCase("greenmail")) {
+		
+			log.info("************************STARTING GREENMAIL****************");
+			greenMail = new GreenMail(ServerSetupTest.SMTP);
+			greenMail.start();
+			
+		}
+		return;
 	}
 
 }
