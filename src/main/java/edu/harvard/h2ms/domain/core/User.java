d57bd7660b19761dcb87f3268a9361f8446a085a
shinwachi <shinwachi@gmail.com>
2018-03-30 23:01:05 -0400

    Update User.java
    removed "enabled" comment.

--- src/main/java/edu/harvard/h2ms/domain/core/User.java
+++ src/main/java/edu/harvard/h2ms/domain/core/User.java
@@ -206,7 +206,7 @@ public class User implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true; //enabled;
+        return true;
     }
     
     public void setEnabled(boolean value) {
