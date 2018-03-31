ab86da3adb0b5f3451d6d748a17ef138202d40c9
Wesley Skillern <wskillern@g.harvard.edu>
2018-03-30 21:59:49 -0500

    - patternfly include location testing in progress - commiting to checkout ben's branch

--- frontend/src/app/reports/reports.component.ts
+++ frontend/src/app/reports/reports.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.css']
+  styleUrls: ['./reports.component.css',
+    '../../../node_modules/patternfly/dist/css/patternfly.min.css',
+    '../../../node_modules/patternfly/dist/css/patternfly-additions.min.css']
 })
 export class ReportsComponent implements OnInit {
 
