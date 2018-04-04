d97d36f69659ac3599c4d2bff908ff1555850e0c
Wesley Skillern <wskillern@g.harvard.edu>
2018-04-04 19:07:09 -0400

    - updated report component to use config - disabled fake data for demo - cleaned up unnecessary comments in reports component - removed login card spacing - style: changed some double quotes to single quotes

--- frontend/src/app/reports/reports.service.ts
+++ frontend/src/app/reports/reports.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {ConfigService} from "../config/config.service";
-import {Config} from "../config/config";
+import {ConfigService} from '../config/config.service';
+import {Config} from '../config/config';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
