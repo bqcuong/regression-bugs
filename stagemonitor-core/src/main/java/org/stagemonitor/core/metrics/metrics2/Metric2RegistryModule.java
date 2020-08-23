4835cb29dccf65c4ea1d15aae8bff9884dcc1ea3
bqcuong <bqcuong2212@gmail.com>
2020-08-23 15:43:00 +0700

    update

--- stagemonitor-core/src/main/java/org/stagemonitor/core/metrics/metrics2/Metric2RegistryModule.java
+++ stagemonitor-core/src/main/java/org/stagemonitor/core/metrics/metrics2/Metric2RegistryModule.java
@@ -164,9 +164,9 @@ public class Metric2RegistryModule extends Module {
 		writeDoubleUnlessNaN(jg, "min", convertDuration(snapshot.getMin()));
 		writeDoubleUnlessNaN(jg, "max", convertDuration(snapshot.getMax()));
 		writeDoubleUnlessNaN(jg, "mean", convertDuration(snapshot.getMean()));
-		writeDoubleUnlessNaN(jg, "median", convertDuration(snapshot.getMedian()));
 		writeDoubleUnlessNaN(jg, "std", convertDuration(snapshot.getStdDev()));
 		writeDoubleUnlessNaN(jg, "p25", convertDuration(snapshot.getValue(0.25)));
+		writeDoubleUnlessNaN(jg, "p50", convertDuration(snapshot.getMedian()));
 		writeDoubleUnlessNaN(jg, "p75", convertDuration(snapshot.get75thPercentile()));
 		writeDoubleUnlessNaN(jg, "p95", convertDuration(snapshot.get95thPercentile()));
 		writeDoubleUnlessNaN(jg, "p98", convertDuration(snapshot.get98thPercentile()));
