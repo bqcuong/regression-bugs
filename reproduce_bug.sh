345168c753eddbcf96947f14636ebc4ba191c013
bqcuong <bqcuong2212@gmail.com>
2020-08-29 23:22:41 +0700

    update reproduce_bug.sh

--- reproduce_bug.sh
+++ reproduce_bug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mvn -Dhttps.protocols=TLSv1.2 -Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true -Denforcer.skip=true -DskipTests clean install -pl navigation-formats
+mvn -Dhttps.protocols=TLSv1.2 -Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true -Denforcer.skip=true -DskipTests install -pl common -pl navigation-formats
 
-mvn dependency:copy-dependencies -pl navigation-formats
+mvn dependency:copy-dependencies -pl common -pl navigation-formats
 rm navigation-formats/target/dependency/common-2.25-SNAPSHOT.jar
