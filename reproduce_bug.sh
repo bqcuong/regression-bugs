0bf38252551734d8cb9b1fb39d8731828d9270e3
bqcuong <bqcuong2212@gmail.com>
2020-08-29 18:19:49 +0700

    update reproduce_bug.sh

--- reproduce_bug.sh
+++ reproduce_bug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn -Dhttps.protocols=TLSv1.2 -Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true -Denforcer.skip=true clean test-compile -pl byte-buddy-dep
+mvn -Dhttps.protocols=TLSv1.2 -Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true -Denforcer.skip=true -DskipTests install -pl byte-buddy-dep
 mvn dependency:copy-dependencies -pl byte-buddy-dep
