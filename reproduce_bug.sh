71af89bcf4c48692a682f86286a6f603b127a230
bqcuong <bqcuong2212@gmail.com>
2020-08-30 13:22:23 +0700

    update reproduce_bug.sh

---
+++ reproduce_bug.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mvn -Dhttps.protocols=TLSv1.2 -Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true -Denforcer.skip=true -DskipTests clean install
+mvn dependency:copy-dependencies -pl core -pl suite/spdz
